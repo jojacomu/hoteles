41bde3d21d50b7bf7564d2e10b507cc8cd7005c2
johnCortes <jojacomu@gmail.com>
2023-07-31 00:09:28 -0500

    "Mejora de presentación de las card y llamado precios de hoteles"

--- src/hoteles.js
+++ src/hoteles.js
@@ -1,13 +1,13 @@
 import { consultarHoteles } from "./apihotels.js";
-const main = document.querySelector(".hotels-container");
+const sectionHotels = document.querySelector(".hotels-container");
 const respuesta2 = await consultarHoteles();
 const data = await respuesta2.json();
 console.log(data);
-main.innerHTML = "";
+sectionHotels.innerHTML = "";
 data.forEach((hotel) => {
     const cardHotel = document.createElement("div");
     cardHotel.className = "card";
-    main.appendChild(cardHotel);
+    sectionHotels.appendChild(cardHotel);
     const imagenHotel = document.createElement("img");
     imagenHotel.setAttribute("src", hotel.photo);
     imagenHotel.setAttribute("alt", hotel.name);
@@ -17,23 +17,28 @@ data.forEach((hotel) => {
     nombreHotel.innerText = hotel.name;
     nombreHotel.className = "hotel-name";
     cardHotel.appendChild(nombreHotel);
-    const textoPais = document.createElement("span");
-    textoPais.innerHTML = "Pais"
-    textoPais.className = "paises";
-    cardHotel.appendChild(textoPais);
     const paisHotel = document.createElement("p");
     paisHotel.innerText = hotel.country;
     paisHotel.className = "hotel-pais";
     cardHotel.appendChild(paisHotel);
-    const textoRooms = document.createElement("span");
-    textoRooms.innerHTML = "Rooms"
-    textoRooms.className = "rooms";
-    cardHotel.appendChild(textoRooms);
+    const textoCard = document.createElement("div")
+    cardHotel.appendChild(textoCard);
+    textoCard.className = "textoCard"
     const roomsHotel = document.createElement("p");
     roomsHotel.innerText = hotel.rooms;
     roomsHotel.className = "hotel-rooms";
-    cardHotel.appendChild(roomsHotel);
+    textoCard.appendChild(roomsHotel);
+    const textoRooms = document.createElement("p");
+    textoRooms.innerHTML = "rooms"
+    textoRooms.className = "rooms";
+    textoCard.appendChild(textoRooms);
+    const priceHotel = document.createElement("p");
+    priceHotel.innerText = hotel.price;
+    priceHotel.className = "hotel-price";
+    cardHotel.appendChild(priceHotel);
 });
 
-// Llamamos a la función para mostrar las publicaciones cuando la página se cargue
-window.onload = consultarHoteles;
+// // Llamamos a la función para mostrar las publicaciones cuando la página se cargue
+// window.onload = consultarHoteles;
+
+// const filtroInput = document.getElementById('filtro-titulo');
