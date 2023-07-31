30f90102af5abb9098e1f40ec838da2883742a67
johnCortes <jojacomu@gmail.com>
2023-07-30 21:25:52 -0500

    "Activación de cards al cargue de la info de la api"

--- src/hoteles.js
+++ src/hoteles.js
@@ -1,41 +1,39 @@
 import { consultarHoteles } from "./apihotels.js";
-
-const buttonConsulta = document.getElementById("clear");
 const main = document.querySelector(".hotels-container");
-
-buttonConsulta.addEventListener("click", async () => {
-    const respuesta = await consultarHoteles();
-    const data = await respuesta.json();
-    console.log(data);
-    main.innerHTML = "";
-    data.forEach((hotel) => {
-        const cardHotel = document.createElement("div");
-        cardHotel.className = "card";
-        main.appendChild(cardHotel);
-        const imagenHotel = document.createElement("img");
-        imagenHotel.setAttribute("src", hotel.photo);
-        imagenHotel.setAttribute("alt", hotel.name);
-        imagenHotel.className = "hotel-image";
-        cardHotel.appendChild(imagenHotel);
-        const nombreHotel = document.createElement("h2");
-        nombreHotel.innerText = hotel.name;
-        nombreHotel.className = "hotel-name";
-        cardHotel.appendChild(nombreHotel);
-        const textoPais = document.createElement("span");
-        textoPais.innerHTML = "Pais"
-        textoPais.className = "paises";
-        cardHotel.appendChild(textoPais);
-        const paisHotel = document.createElement("p");
-        paisHotel.innerText = hotel.country;
-        paisHotel.className = "hotel-pais";
-        cardHotel.appendChild(paisHotel);
-        const textoRooms = document.createElement("span");
-        textoRooms.innerHTML = "Rooms"
-        textoRooms.className = "rooms";
-        cardHotel.appendChild(textoRooms);
-        const roomsHotel = document.createElement("p");
-        roomsHotel.innerText = hotel.rooms;
-        roomsHotel.className = "hotel-rooms";
-        cardHotel.appendChild(roomsHotel);
-        });
+const respuesta2 = await consultarHoteles();
+const data = await respuesta2.json();
+console.log(data);
+main.innerHTML = "";
+data.forEach((hotel) => {
+    const cardHotel = document.createElement("div");
+    cardHotel.className = "card";
+    main.appendChild(cardHotel);
+    const imagenHotel = document.createElement("img");
+    imagenHotel.setAttribute("src", hotel.photo);
+    imagenHotel.setAttribute("alt", hotel.name);
+    imagenHotel.className = "hotel-image";
+    cardHotel.appendChild(imagenHotel);
+    const nombreHotel = document.createElement("h2");
+    nombreHotel.innerText = hotel.name;
+    nombreHotel.className = "hotel-name";
+    cardHotel.appendChild(nombreHotel);
+    const textoPais = document.createElement("span");
+    textoPais.innerHTML = "Pais"
+    textoPais.className = "paises";
+    cardHotel.appendChild(textoPais);
+    const paisHotel = document.createElement("p");
+    paisHotel.innerText = hotel.country;
+    paisHotel.className = "hotel-pais";
+    cardHotel.appendChild(paisHotel);
+    const textoRooms = document.createElement("span");
+    textoRooms.innerHTML = "Rooms"
+    textoRooms.className = "rooms";
+    cardHotel.appendChild(textoRooms);
+    const roomsHotel = document.createElement("p");
+    roomsHotel.innerText = hotel.rooms;
+    roomsHotel.className = "hotel-rooms";
+    cardHotel.appendChild(roomsHotel);
 });
+
+// Llamamos a la función para mostrar las publicaciones cuando la página se cargue
+window.onload = consultarHoteles;
