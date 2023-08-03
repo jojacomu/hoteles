521896f76f5e744edff9439358f2591439eddf44
johnCortes <jojacomu@gmail.com>
2023-08-02 19:37:02 -0500

    "Pruebas de filtrado por país"

--- src/hoteles.js
+++ src/hoteles.js
@@ -26,8 +26,18 @@ data.forEach((hotel) => {
 });
 
 // *******************************************Filtro por país ************************************
-
-
+function pais() {
+    const selectCountry = document.getElementById("country");
+    selectCountry.addEventListener("change", (event)=>{
+    // La función se ejecutará cuando el usuario seleccione una opción
+        const countrySelected = event.target.value;
+        console.log('Opción seleccionada:', countrySelected);
+            return countrySelected;
+    // Aquí puedes realizar acciones adicionales según el valor seleccionado
+})
+}
+const respuesta3 = pais();
+console.log(respuesta3)
 
 function obtenerHotelesEnArgentina() {
     const url = 'https://6256097e8646add390e01d99.mockapi.io/hotels/reservation/hotels';
@@ -40,14 +50,59 @@ function obtenerHotelesEnArgentina() {
         return response.json();
     })
     .then(data => {
-        // Filtramos los objetos que tienen "country" con el valor "Argentina"
+        // function mostrarHoteles(hoteles) {
+        //     // Obtener el elemento donde se mostrará la información
+        //     const sectionHotels = document.querySelector(".hotels-container");
+        
+        //     // Limpiar el contenido previo
+        //     sectionHotels.innerHTML = '';
+        
+        //     // Mostrar cada hotel en la página
+        //     hoteles.forEach(hotel => {
+        //     const cardHotel = document.createElement("div");
+        //     cardHotel.className = "card";
+        //     cardHotel.innerHTML = `
+        //         <img src="${hotel.photo}" alt="${hotel.name}" class="hotel-image" />
+        //         <h2>${hotel.name}</h2>
+        //         <div class="detallesCard">
+        //             <p>País: ${hotel.country}</p>
+        //             <div class="textoCard">
+        //                 <p>Rooms: ${hotel.rooms}</p>
+        //                 <p>Price: ${hotel.price}</p>
+        //                 <div>
+        //                     <button class="hotel-bookit clear">Book it!</button>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <!-- Agregar más detalles del hotel si es necesario -->
+        //     `;
+        //     sectionHotels.appendChild(cardHotel);
+        //     });
+        // }
+        // // Filtramos los objetos que tienen "country" con el valor "Argentina"
+        // if(respuesta3 === 'Argentina') {
+        //     const hotelesEnArgentina = data.filter(hotel => hotel.country === 'Argentina');
+        //     mostrarHoteles(hotelesEnArgentina);
+        // } if(respuesta3 === 'Brasil') {
+        //     const hotelesEnBrasil = data.filter(hotel => hotel.country === 'Brasil');
+        //     mostrarHoteles(hotelesEnBrasil);
+        // } if(respuesta3 === 'Chile') {
+        //     const hotelesEnBrasil = data.filter(hotel => hotel.country === 'Chile');
+        //     mostrarHoteles(hotelesEnChile);
+        // } if(respuesta3 === 'Uruguay') {
+        //     const hotelesEnBrasil = data.filter(hotel => hotel.country === 'Uruguay');
+        //     mostrarHoteles(hotelesEnUruguay);
+        // } else {
+        //     alert(`No hay ningún hotel disponible para ${respuesta3}`);
+        // }
         const hotelesEnArgentina = data.filter(hotel => hotel.country === 'Argentina');
         const hotelesEnBrasil = data.filter(hotel => hotel.country === 'Brasil');
         const hotelesEnChile = data.filter(hotel => hotel.country === 'Chile');
         const hotelesEnUruguay = data.filter(hotel => hotel.country === 'Uruguay');
+
         // Array con los filtros por país
         const hotelsByCountry = [hotelesEnArgentina, hotelesEnBrasil, hotelesEnChile,hotelesEnUruguay] 
-        console.log(hotelsByCountry)
+        // console.log(hotelsByCountry)
         
         // Mostramos la información de los hoteles en la página
         
